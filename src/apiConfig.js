c6ea6d574aeeffcebcdccf93c7d50b74fce41990
Kerimsekili <kerimsekilix@gmail.com>
2024-02-06 18:36:21 +0300

    Adding Alert Context & Reducer

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,5 +1,4 @@
 export const apiConfig = {
   REACT_APP_GITHUB_URL: "https://api.github.com",
-  REACT_APP_GITHUB_TOKEN:
-    "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",
+  REACT_APP_GITHUB_TOKEN: "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",
 };
